7f7ee3b27286fa743f8de526b4b31350a16f6696
mpeciakk <mpeciakk@gmail.com>
2021-05-06 23:49:11 +0200

    Basic ugly navbar

---
+++ src/components/Nav.svelte
@@ -0,0 +1,46 @@
+<nav class="h-12 fixed w-full flex justify-center">
+    <ul class="m-0 p-0 h-full w-2/4 flex">
+        <li class="w-24 h-full">
+            <a
+                href="/"
+                class="text-white no-underline flex h-full flex justify-center items-center p-0 py-4 text-base"
+            >
+                Link1
+            </a>
+        </li>
+        <li class="w-24 h-full">
+            <a
+                href="/"
+                class="text-white no-underline flex h-full flex justify-center items-center p-0 py-4 text-base"
+            >
+                Link2
+            </a>
+        </li>
+        <li class="w-24 h-full">
+            <a
+                href="/"
+                class="text-white no-underline flex h-full flex justify-center items-center p-0 py-4 text-base"
+            >
+                Link3
+            </a>
+        </li>
+        <li class="w-24 h-full bg-red-500 ml-auto">
+            <a
+                href="/"
+                class="text-white no-underline flex h-full flex justify-center items-center p-0 py-4 text-base justify-self-end"
+            >
+                Log out
+            </a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        background-color: #495057;
+    }
+
+    li:hover {
+        background: #343a40;
+    }
+</style>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,9 @@
+<script>
+    import Nav from '../components/Nav.svelte'
+</script>
+
+<Nav />
+
 <main class="h-full w-full flex justify-center items-center bg-gray-200">
     <slot />
 </main>
