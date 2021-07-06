6a00228677d18d11270c5b5d181a248802d1909d
mpeciakk <mpeciakk@gmail.com>
2021-07-06 19:38:40 +0200

    Small post layout improvements & netlify integration

--- src/components/post/Comment.svelte
+++ src/components/post/Comment.svelte
@@ -1,26 +1,30 @@
-<div class="flex w-full px-4 py-2 sm:p-12 sm:py-2 m-0 bg-post-section text-sm">
-    <img
-        class="author-avatar mr-2 rounded"
-        src="https://cdn.discordapp.com/avatars/766005840154853430/2fb8c06f538d6b155e6ad9cca3c3e7d2.png"
-        alt="Avatar"
-    />
+<div class="comment w-full px-12 p-3 bg-post-section text-sm">
+    <div class="post-author flex text-sm">
+        <img
+            class="author-avatar mr-2 sm:rounded"
+            src="https://cdn.discordapp.com/avatars/766005840154853430/2fb8c06f538d6b155e6ad9cca3c3e7d2.png"
+            alt="Avatar"
+        />
 
-    <div class="flex-col w-full">
-        <div class="font-bold">Lorem, ipsum.</div>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Unde in animi repudiandae reiciendis earum laborum rem.
-            Nihil quia officia earum excepturi odio fugiat ad ducimus at
-            ab, tenetur iste debitis, animi quod omnis aperiam adipisci
-            iusto! Numquam, libero possimus. Nam dicta corporis enim
-            numquam, nesciunt rem eius totam est laboriosam?
-        </p>
+        <div class="w-full flex items-center">
+            <div class="font-semibold">Username</div>
+            <div class="px-2">•</div>
+            <div>2 days ago</div>
+        </div>
     </div>
+
+    <p class="pl-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde in animi
+        repudiandae reiciendis earum laborum rem. Nihil quia officia earum
+        excepturi odio fugiat ad ducimus at ab, tenetur iste debitis, animi quod
+        omnis aperiam adipisci iusto! Numquam, libero possimus. Nam dicta
+        corporis enim numquam, nesciunt rem eius totam est laboriosam?
+    </p>
 </div>
 
 <style>
     .author-avatar {
-        width: 40px;
-        height: 40px;
+        width: 24px;
+        height: 24px;
     }
 </style>
--- src/components/post/Post.svelte
+++ src/components/post/Post.svelte
@@ -6,27 +6,32 @@
 
 <div class="w-full mb-16 shadow-lg">
     <div class="border">
-        <div
-            class="flex w-full items-center sm:px-12 sm:py-2 m-0 bg-post-section border-b"
-        >
-            <img
-                class="author-avatar mr-2 sm:rounded"
-                src="https://cdn.discordapp.com/avatars/766005840154853430/2fb8c06f538d6b155e6ad9cca3c3e7d2.png"
-                alt="Avatar"
-            />
+        <div class="w-full sm:px-12 sm:py-2 m-0 bg-post-section border-b">
+            <div class=" flex text-sm">
+                <img
+                    class="author-avatar mr-2 sm:rounded"
+                    src="https://cdn.discordapp.com/avatars/766005840154853430/2fb8c06f538d6b155e6ad9cca3c3e7d2.png"
+                    alt="Avatar"
+                />
 
-            <div class="flex-col w-full">
-                <div class="font-bold">Lorem, ipsum.</div>
-                <div>2 days ago</div>
+                <div class="w-full flex flex-col justify-center">
+                    <div class="font-semibold">Username</div>
+                    <!-- <div class="px-2">•</div> -->
+                    <div>2 days ago</div>
+                </div>
             </div>
 
-            <div class="justify-self-end w-24 sm:text-right">
+            <div class="py-1 mt-1 font-semibold text-2xl">
+                Lorem ipsum dolor sit amet consectetur.
+            </div>
+            <!-- 
+            <div class="flex justify-end items-center w-full post-options">
                 <Icon class="mr-4" data={plus} scale="1.2" />
                 <Icon data={minus} scale="1.2" />
-            </div>
+            </div> -->
         </div>
 
-        <div class="px-4 py-2 sm:p-12 sm:py-2 border-b">
+        <div class="px-4 py-4 sm:p-12 sm:py-6 border-b">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi vel
             asperiores eos molestiae iure vitae porro, esse, possimus, delectus
             nihil nam quia nobis id sed dolore eius quisquam enim odio autem
@@ -42,12 +47,50 @@
 
         <Comment />
         <Comment />
+
+        <!-- <div class="w-full flex bg-white">
+            <input
+                class="w-full py-2 px-4 bg-post-section text-black font-semibold focus:outline-none border border-r-0 border-black"
+                id="comment"
+                type="text"
+                placeholder="Napisz komentarz"
+            />
+
+            <button class="button" type="button">
+                <div class="text">Skomentuj</div>
+            </button>
+        </div> -->
     </div>
 </div>
 
 <style>
     .author-avatar {
-        width: 48px;
-        height: 48px;
+        width: 40px;
+        height: 40px;
     }
+
+    /* .button {
+        @apply relative z-0 bg-login-button text-white font-semibold py-2 px-4 focus:outline-none border border-black;
+    }
+
+    .button::before {
+        content: '';
+        background-color: black;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: -1;
+        transition: all 0.5s ease;
+        width: 0;
+        height: 100%;
+    }
+
+    .button:hover::before {
+        width: 100%;
+    }
+
+    .text {
+        mix-blend-mode: difference;
+    } */
 </style>
